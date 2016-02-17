4f87d129a92f1d7b86a5d762b93243c3dc9c4778
cph <cph@titan.r444b.ee.ntu.edu.tw>
2016-02-17 18:55:02 +0800

    sth new

---
+++ run1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier
+
+
+def main():
+    df_train = pd.read_csv("train.csv")
+    df_test = pd.read_csv("test.csv")
+
+
+if __name__ == "__main__":
+    main()
